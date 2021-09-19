b43de0cef059480a3b36e42fb8526183a7ae67ee
bandhavi47 <bandhavi47@gmail.com>
2021-09-18 21:28:03 -0500

    logging added

--- or.py
+++ or.py
@@ -3,10 +3,17 @@ from utils.all_utils import prepare_data,save_model,save_plot
 
 import pandas as pd
 #import numpy as np
+import logging
+import os
+
+logging_str = "[%(asctime)s - %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename = os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str,filemode='a')
 
 def main(data, eta, epochs,filename,plotFileName):
     df = pd.DataFrame(data)
-    print(df)
+    logging.info(f"This is actual dataframe{df}")
 
     X,y = prepare_data(df)
 
@@ -28,4 +35,10 @@ if __name__ == "__main__": # Entry point
     }
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
-    main(data=OR, eta=ETA, epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+    try:
+        logging.info(">>>> Starting>>>>>>>")
+        main(data=OR, eta=ETA, epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+        logging.info("<<<training done<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
+import logging
 
 plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
 
@@ -16,6 +17,7 @@ def prepare_data(df):
   Returns:
       tuple: it returns the tuples of dependent variables and independent features
   """
+  logging.info("Preparing the data by segregating the dependent variables  and independdent variables")
   X = df.drop("y", axis=1)
   y = df["y"]
   return X, y
@@ -27,13 +29,16 @@ def save_model(model, filename):
       model (python object): trained model to be saved
       filename (str): path to save the trained model
   """
+  logging.info("Saving the trained model")
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
+  logging.info(f"saved the trained model {filePath}")
 
 def save_plot(df, file_name, model):
   def _create_base_plot(df):
+    logging.info("creating the base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -41,6 +46,7 @@ def save_plot(df, file_name, model):
     figure.set_size_inches(10, 8)
 
   def _plot_decision_regions(X, y, classfier, resolution=0.02):
+    logging.info("decision boundary")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -52,8 +58,8 @@ def save_plot(df, file_name, model):
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
                            np.arange(x2_min, x2_max, resolution))
-    print(xx1)
-    print(xx1.ravel())
+    #print(xx1)
+    #print(xx1.ravel())
     Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
@@ -73,3 +79,4 @@ def save_plot(df, file_name, model):
   os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   plotPath = os.path.join(plot_dir, file_name) # model/filename
   plt.savefig(plotPath) 
+  logging.info(f"{plotPath}")
